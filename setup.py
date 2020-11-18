25a5cad05b9466f03a7f9f138a6937e5e05187f9
Thomas <thomas.carbillet@gmail.com>
2020-11-18 10:39:03 +0100

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="briefme-indexable-feed",
+    version="0.0.1",
+    description="indexable-feed base for Brief.me projects",
+    url="https://github.com/briefmnews/briefme-editorial",
+    author="Brief.me",
+    author_email="tech@brief.me",
+    packages=[
+        "briefme_indexable_feed"
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "Django>=2.2",
+        "djangorestframework>=3.10.0",
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
