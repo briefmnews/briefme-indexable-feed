eb3efc6d6beecf976b0572e69b5c498ca6db4383
Thomas <thomas.carbillet@gmail.com>
2022-02-04 08:21:03 +0100

    fix: import version in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+from briefme_indexable_feed import __version__
+
 setup(
     name="briefme-indexable-feed",
-    version="0.1",
+    version=__version__,
     description="indexable-feed base for Brief.me projects",
     url="https://github.com/briefmnews/briefme-editorial",
     author="Brief.me",
